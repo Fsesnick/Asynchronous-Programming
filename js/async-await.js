f85abed27511311b2eb995751bd15bddd4703e3d
Fsesnick <61622322+Fsesnick@users.noreply.github.com>
2021-03-04 17:48:28 +0100

    Error Handling with try...catch

--- js/async-await.js
+++ js/async-await.js
@@ -4,14 +4,21 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // Lida com todas as solicitações de busca 
+async function getJSON(url) {
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+}
+
 async function getPeopleInSpace(url) {
-  const peopleResponse = await fetch(url);
-  const peopleJSON = await peopleResponse.json();
+  const peopleJSON = await getJSON(url);
 
   const profiles = peopleJSON.people.map( async (person) => {
     const craft = person.craft;
-    const profileResponse = await fetch(wikiUrl + person.name);
-    const profileJSON = await profileResponse.json();
+    const profileJSON = await getJSON(wikiUrl + person.name);
 
     return { ...profileJSON, craft };
   });
@@ -24,7 +31,7 @@ function generateHTML(data) {
   data.map( person => {
     const section = document.createElement('section');
     peopleList.appendChild(section);
-    //Verifique se a solicitação retorna uma página 'padrão' do Wiki 
+ //Verifique se a solicitação retorna uma página 'padrão' do Wiki 
     if (person.type === 'standard') {
       section.innerHTML = `
         <img src=${person.thumbnail.source}>
@@ -49,5 +56,9 @@ btn.addEventListener('click', (event) => {
 
   getPeopleInSpace(astrosUrl)
     .then(generateHTML)
+    .catch( e => {
+      peopleList.innerHTML = '<h3>Algo deu errado!</h3>';
+      console.error(e);
+    })
     .finally( () => event.target.remove() )
 });
